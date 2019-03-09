63149fff9ac3dd0441d86a1e7cb19eaa566c87b2
xiaotaohe <taochao1997@qq.com>
2019-03-09 19:17:40 +0800

    三步翻转法

--- 句子翻转.cpp
+++ 句子翻转.cpp
@@ -2,33 +2,28 @@
 #include<string>
 #include<algorithm>
 using namespace std;
-//单词翻转逻辑
-void _reverse(string& str,int left,int right)
-{
-    while(left<right)
-    {
-        swap(str[left],str[right]);
-        left++;
-        right--;
-    }
-}
+
 int main()
 {
     string str;
     getline(cin,str);
     //整个句子翻转
     reverse(str.begin(),str.end());
-    int left = 0;
+    
     //单词逐个翻转
-    for(int i = 0;i<str.size();i++)
+    string :: iterator start = str.begin();
+    string :: iterator end = str.begin();
+    while(start != str.end())
     {
-       if(str[i] == ' ')
-       {
-           _reverse(str,left,i-1);
-           left = i+1;
-       }
+        end = start;
+        while(end != str.end() && *end != ' ')
+            end++;
+        reverse(start,end);
+        if(end != str.end())
+            start = end+1;
+        else
+            start = end;
     }
-    reverse(str.begin()+left,str.end());
     cout<<str<<endl;
     return 0;
 }
